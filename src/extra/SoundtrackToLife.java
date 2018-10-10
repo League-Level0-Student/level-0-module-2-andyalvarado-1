32d5ff16e58505fdb58fa978e650cd14e12f5855
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-09 17:45:50 -0700

    soundtracklife

--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -12,16 +12,38 @@ public class SoundtrackToLife {
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
 		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "sad", "happy", "angry", "idk", "cool", "pac man" }, null);
 		
 		// 2. Their answer is stored in the userMood variable. Print it out.
-
+System.out.println(userMood);
 		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
+		if(userMood==2) {
+			playVideo("https://www.youtube.com/watch?v=6fVE8kSM43I");
+		}
+		if(userMood==1) {
+			playVideo("https://www.youtube.com/watch?v=1V_xRb0x9aw");
+		}
+		if(userMood==0) {
+			playVideo("https://www.youtube.com/watch?v=CpnPxNN3H8c");
+		}
+		if(userMood==3) {
+			playVideo("https://www.youtube.com/watch?v=YVkUvmDQ3HY");
+		}
 		
-
 		
-		// 4. Play different songs for other moods.
+		if(userMood==4) {
+			playVideo("https://www.youtube.com/watch?v=ToY6sjSV8h8");
 
+		}
+		
+		if(userMood==5) {
+			playVideo("https://www.youtube.com/watch?v=fGqdIPer-ms");
+
+		}
+		
+		
+			
+			
 /**
 * If you can’t think of any, here are some you can use...
 *	http://bit.ly/video-for-happy
