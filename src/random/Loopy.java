37ae21e7d728b7df18cb9f90d2a8d27fb71c1755
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-13 17:33:12 -0800

    loopy

---
+++ src/random/Loopy.java
@@ -0,0 +1,51 @@
+package random;
+
+import java.util.Random;
+
+public class Loopy {
+public static void main(String[] args) {
+
+	Random rey =new Random();
+int randomChoice = rey.nextInt(1000);
+System.out.println(randomChoice);	
+int random = rey.nextInt(900);
+System.out.println(random);	
+	
+System.out.println(random-randomChoice);	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
